585c252f5b9d8f07d5c88be0527714741f59c031
Veizyl <sturner@mail.bradley.edu>
2024-03-21 22:38:06 -0500

    ResourceManager done?

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -4,84 +4,116 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    [SerializeField] int reputationAmount;
-    [SerializeField] int goldAmount;
-    [SerializeField] int healthAmount;
-    [SerializeField] int crewAmount;
-    [SerializeField] int reputationChange;
-    [SerializeField] int goldChange;
-    [SerializeField] int healthChange;
-    [SerializeField] int crewChange;
+    // Serialized fields to set the initial values for different resources in the Unity Inspector.
+    [SerializeField] int startingReputation = 0;
+    [SerializeField] int startingGold = 100;
+    [SerializeField] int startingHealth = 100;
+    [SerializeField] int startingCrew = 10;
+
+    // Variables to store the current amounts of different resources.
+    [SerializeField]  private int reputationAmount;
+    [SerializeField] private int goldAmount;
+    [SerializeField] private int healthAmount;
+    [SerializeField] private int crewAmount;
+
+    // Constants to define the minimum and maximum values for certain resources.
+    private const int minReputation = -100;
+    private const int maxReputation = 100;
+    private const int maxHealth = 100;
 
     private void Start()
     {
-        print("Gold: " + goldAmount);
-        print("Health: " + healthAmount);
-        print("Crew: " + crewAmount);
-
-        ReputationAdjust(reputationChange);
-        GoldAdjust(goldChange);
-        HealthAdjust(healthChange);
-        CrewAdjust(crewChange);
+        InitializeResources();
+        CheckResources();
     }
 
-    public void ReputationAdjust(int amount)
+    // Method to initialize the resources with their starting values.
+    private void InitializeResources()
     {
-        reputationAmount += amount;
-        if ((reputationAmount + amount) <= -100)
+        reputationAmount = startingReputation;
+        goldAmount = startingGold;
+        healthAmount = startingHealth;
+        crewAmount = startingCrew;
+    }
+
+    // Method to log the current values of different resources.
+    private void CheckResources()
+    {
+        Debug.Log("Reputation: " + reputationAmount);
+        Debug.Log("Gold: " + goldAmount);
+        Debug.Log("Health: " + healthAmount);
+        Debug.Log("Crew: " + crewAmount);
+    }
+
+    // Method to adjust the reputation by a specified amount.
+    public void AdjustReputation(int amount)
+    {
+        // Clamps the new reputation value between minReputation and maxReputation.
+        int newReputation = Mathf.Clamp(reputationAmount + amount, minReputation, maxReputation);
+        // If the reputation has changed, update it and log appropriate messages.
+        if (newReputation != reputationAmount)
         {
-            reputationAmount = -100;
-            print("Your reputation has reached -100");
+            reputationAmount = newReputation;
+            if (newReputation == minReputation)
+            {
+                Debug.Log("Your reputation has reached minimum (" + minReputation + ")");
+            }
+            else if (newReputation == maxReputation)
+            {
+                Debug.Log("Your reputation has reached maximum (" + maxReputation + ")");
+            }
+            else
+            {
+                Debug.Log("Your reputation is: " + reputationAmount);
+            }
         }
-        else if((reputationAmount + amount) >= 100 )
+    }
+
+    // Method to adjust the amount of gold by a specified amount.
+    public void AdjustGold(int amount)
+    {
+        // Checks if there is enough gold to perform the action.
+        if (goldAmount + amount < 0)
         {
-            reputationAmount = 100;
-            print("Your reputation has reached 100");
+            Debug.Log("You do not have enough gold to perform this action.");
         }
         else
         {
-            print("Your reputation is: " + reputationAmount);
-        }
-
-    }
-    public void GoldAdjust(int amount)
-    {
-        if((goldAmount + amount) < 0)
-        {
-            print("You do not have enough money to purchase this.");
-        }
-        else
-        {
+            // If there is enough gold, adjust it and log the new amount.
             goldAmount += amount;
-            print("You now have " + goldAmount + " gold");
+            Debug.Log("You now have " + goldAmount + " gold");
         }
-
     }
-    public void HealthAdjust(int amount)
 
+    // Method to adjust the health by a specified amount.
+    public void AdjustHealth(int amount)
     {
-        if((healthAmount + amount) <= 0)
+        // Clamps the health within the range of 0 to maxHealth to prevent negative health.
+        healthAmount = Mathf.Clamp(healthAmount + amount, 0, maxHealth); //ensures health will never go below 0
+        // Log death message or the new health amount.
+        if (healthAmount == 0)
         {
-            healthAmount = 0;
-            print("You have died");
+            Debug.Log("You have died");
         }
         else
         {
-            healthAmount += amount;
-            print("You now have " + healthAmount + " health");
+            Debug.Log("You now have " + healthAmount + " health");
         }
     }
-    public void CrewAdjust(int amount)
+
+    // Method to adjust the number of crew members by a specified amount.
+    public void AdjustCrew(int amount)
     {
-        if ((crewAmount + amount) <= 0)
+        // Ensures the crew count doesn't go below 0.
+        crewAmount = Mathf.Max(crewAmount + amount, 0);
+        // Log all crew lost or the new crew count.
+        if (crewAmount == 0)
         {
-            crewAmount = 0;
-            print("You have lost all your crew");
+            Debug.Log("You have lost all your crew");
         }
         else
         {
-            crewAmount += amount;
-            print("You now have " + crewAmount + " crew");
+            Debug.Log("You now have " + crewAmount + " crew");
         }
     }
 }
