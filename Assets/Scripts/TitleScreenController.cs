0c05100ce5a40e40b251407243866827becbd45a
Fawful4Ever <114947591+Fawful4Ever@users.noreply.github.com>
2024-03-26 14:56:26 -0500

    TitleScreen Created
    Basic title screen was created with a script containing functions for it, as well as a how to play explaining to the player how to play the game, as well as win and lose conditions

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreenController : MonoBehaviour
+{
+    [SerializeField] private GameObject HowToPlay;
+    [SerializeField] private GameObject MenuButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HowToPlay.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenHowToPlay()
+    {
+        HowToPlay.SetActive(true);
+        MenuButtons.SetActive(false);
+    }
+
+    public void CloseHowToPlay()
+    {
+        HowToPlay.SetActive(false);
+        MenuButtons.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
